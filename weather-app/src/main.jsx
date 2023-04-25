7a9b8dbde80e6c49f87478c72ab7318c890b14aa
Camilla Gustafsson <camilla.gustafsson@chasacademy.com>
2023-04-25 13:51:50 +0200

    update

--- weather-app/src/main.jsx
+++ weather-app/src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+const app = (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+const rootElement = createRoot(root);
+rootElement.render(app);
